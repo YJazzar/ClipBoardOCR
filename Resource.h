1262ff0f8a9db8c095698307c60400cb559352d5
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-09-30 03:45:15 -0500

    Changed the application into a Win32 App

--- Resource.h
+++ Resource.h
@@ -1,17 +1,18 @@
+#pragma once
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by ClipBoardOCR.rc
+// Used by ClipboardOCR2.rc
 
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_CLIPBOARDOCR_DIALOG	102
+#define IDD_CLIPBOARDOCR2_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_CLIPBOARDOCR			107
+#define IDI_CLIPBOARDOCR2			107
 #define IDI_SMALL				108
-#define IDC_CLIPBOARDOCR			109
+#define IDC_CLIPBOARDOCR2			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
